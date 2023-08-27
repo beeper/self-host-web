6354efe81454a62c765e1da7db0fee94aca1f525
Jakub Doboš <26146502+kubo6472@users.noreply.github.com>
2023-08-27 19:31:12 +0200

    Add the remaining bridges that should work with Fly.io

--- app/components/BridgeDeploy.tsx
+++ app/components/BridgeDeploy.tsx
@@ -48,7 +48,11 @@ export default function BridgeDeploy({beeperToken, flyToken, onCreate}: any) {
         discord: "Discord",
         slack: "Slack",
         telegram: "Telegram",
-        twitter: "Twitter"
+        twitter: "Twitter",
+        heisenbridge: "HeisenBridge (IRC)",
+        facebook: "Facebook",
+        instagram: "Instagram",
+        imessage: "iMessage"
     }
 
 
